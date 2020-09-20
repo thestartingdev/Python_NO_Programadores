e13811a93fad33402890de7670b9aaed3b15f9e5
Juan P. Romano <programming@jpromano.net>
2020-09-20 15:32:35 -0300

    ¿Cuantos mundiales viviste por año?

---
+++ Workshop_02/mundialesPorAño/cuantosMundiales.py
@@ -0,0 +1,13 @@
+edad = int(input ("Contame cuántos años tenés, y te digo cuántos mundiales viviste :"))
+print ("Qué groso/a, viviste", (edad // 4) ,"mundiales")
+if edad > 34:
+    print ("Y encima lo viste jugar a Maradona")
+elif edad > 70:
+    print ("Qué groso/a, viviste 18 mundiales")
+
+elif edad > 82:
+    print ("Qué groso/a, viviste 19 mundiales")
+
+elif edad > 86:
+    print ("Qué groso/a, viviste 20 mundiales")
+
---
+++ Workshop_02/mundialesPorAño/cuantosMundialesPorAño.py
@@ -0,0 +1,4 @@
+año = int(input ("Contame en qué año naciste, y te digo cuántos mundiales viviste :"))
+print ("Qué groso/a, viviste", ((2020 - año) // 4) ,"mundiales")
+if año < 1986:
+    print ("Y encima tuviste la suerte de verlo jugar a Maradona")
